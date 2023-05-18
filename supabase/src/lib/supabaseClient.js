1f9713689016418d824f954580e807e56b743150
ericl2055 <eric_lin2055@icloud.com>
2023-05-18 13:57:08 -0400

    n

--- supabase/src/lib/supabaseClient.js
+++ supabase/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://eoinwwvroxgdtbieyavr.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaW53d3Zyb3hnZHRiaWV5YXZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQzNDYyODIsImV4cCI6MTk5OTkyMjI4Mn0.ZIXTcuAJqLIhTow48WHyf1XCPdEgG6MyWr0jxBrtHCQ'
+  'https://qndxemyshxetvjxakktz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZHhlbXlzaHhldHZqeGFra3R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MzIyODAsImV4cCI6MjAwMDAwODI4MH0.NpFsJQWyt-A7Qa52Co8NECJVCaUCTLysfBXDjSYhmd4'
 )
