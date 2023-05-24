38118544bba544b4d5ef9302ead9a3b29d3a4639
ericl2055 <eric_lin2055@icloud.com>
2023-05-24 18:23:56 -0400

    completed register component

--- supabase/src/App.vue
+++ supabase/src/App.vue
@@ -1,11 +1,24 @@
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <RouterLink to="/" v-if="showRegister">Home</RouterLink>
+    <Account :userId="userId" v-if="session" :session="session" />
+    <Login v-else-if="!showRegister" />
+    <RouterLink to="/register" v-if="!showRegister && !session">Register</RouterLink>
+
+    <RouterView v-if="showRegister" />
+  </div>
+</template>
+
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { onMounted, ref } from 'vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+import { onMounted, ref, watch } from 'vue'
 import Account from './components/Account.vue'
 import Login from './components/Login.vue'
 import { supabase } from '../src/lib/supabaseClient'
 
 const session = ref()
+const route = useRoute()
+const showRegister = ref(false)
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
@@ -15,16 +28,9 @@ onMounted(() => {
   supabase.auth.onAuthStateChange((_, _session) => {
     session.value = _session
   })
+
+  watch(route, () => {
+    showRegister.value = route.path === '/register'
+  })
 })
 </script>
-
-<template>
-  <div class="container" style="padding: 50px 0 100px 0">
-    <RouterLink to="/">Home</RouterLink>
-    <Account :userId="userId" v-if="session" :session="session" />
-    <Login v-else />
-    <RouterLink to="/register">Register</RouterLink>
-
-    <RouterView />
-  </div>
-</template>
